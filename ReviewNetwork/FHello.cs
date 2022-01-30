a4d24d13603e6d539fd0f62fa9d10cba520f33c6
metamorphosa <34010859+metamorphosa@users.noreply.github.com>
2022-01-31 00:06:04 +0300

    Create FHello.cs

---
+++ ReviewNetwork/FHello.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewNetwork
+{
+    public class TestClass
+    {
+        public void ToTest()
+        {
+            Console.WriteLine("hello,world");
+        }
+    }
+}
